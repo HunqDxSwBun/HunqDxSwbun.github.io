10639aecbf2b24c69a2a15eb6016d07ac1911285
HunqDxSwBun <117713475+HunqDxSwBun@users.noreply.github.com>
2022-11-19 03:46:29 +0700

    HunqD

--- Data/JS/Setting.js
+++ Data/JS/Setting.js
@@ -22,6 +22,46 @@ function FnHieuUng() {
     }
     if (OptHieuUng == 1) {
         vHieuUng.style.display = 'block'
-    }
-    
+    }   
 }
+
+
+// Chọn Trang Phục
+  var vIMGCouple = document.getElementById("iIMGCouple"); 
+  function FnSkin() {
+ 
+    var Skin = document.querySelector('input[name="Skin"]:checked').value;
+
+    if (Skin == 0) {
+      vIMGCouple.src =  "./Data/IMG/Skins/Skin1.png";
+    }
+    if (Skin == 1) {
+      vIMGCouple.src = "./Data/IMG/Skins/Skin2.png";
+    }
+    if (Skin == 2) {
+      vIMGCouple.src = "./Data/IMG/Skins/Skin3.png";
+    }
+    if (Skin == 3) {
+      vIMGCouple.src = "./Data/IMG/Skins/Skin4.png";
+    }
+  }
+  
+  // Chọn Hình Nền
+
+  function FnBackground() {
+ 
+    var Background = document.querySelector('input[name="Background"]:checked').value;
+
+    if (Background == 0) {
+      document.body.style.backgroundImage =  "none";
+    }
+    if (Background == 1) {
+      document.body.style.backgroundImage = "url(/Data/IMG/Background/BG1.jpg)";
+    }
+    if (Background == 2) {
+      document.body.style.backgroundImage = "url(/Data/IMG/Background/BG2.jpg)";
+    }
+    if (Background == 3) {
+      document.body.style.backgroundImage = "url(/Data/IMG/Background/BG3.jpeg)";
+    }
+  }
